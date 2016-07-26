641af8ee67554c20e48f8db763d046fd123d6a1e
unknown <>
2016-07-26 10:45:42 +0800

    second

--- 404/a.js
+++ 404/a.js
@@ -1,10 +1,1 @@
-var http = require('http');
-var url = require('url');
-var util = require('util');
-
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(util.inspect(url.parse(req.url, true)));
-}).listen(4000);/**
- * Created by think on 2016/7/25.
- */
+asdkljfklsajklfajsdfklsjflkajsfaslkasdjfasfkalsdjlkasfd
